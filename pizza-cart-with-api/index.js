1f62af7c5e2a4cb9a875cf495a7f6649bbcfb14a
Londeka Zikalala <londekazikalala@gmail.com>
2023-07-21 16:01:43 +0200

    first commit

---
+++ pizza-cart-with-api/index.js
@@ -0,0 +1,21 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('pizzaCartWithAPI', function() {
+        return {
+            title: 'Perfect Pizza',
+            pizzas: [],
+
+            getPizzas() {
+                const url = `https://pizza-api.projectcodex.net/api/pizzas`;
+
+                axios.get(url)
+                    .then(result => {
+                        this.pizzas = result.data.pizzas;
+                        console.log(this.pizzas); // Check the pizzas array content
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+            }
+        }
+    });
+});
